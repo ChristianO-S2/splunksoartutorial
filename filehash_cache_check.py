11853133adc69901bd5796f2607715672bd93e9d
admin <root@localhost>
2022-03-03 03:29:01 +0000

    delete and insert

--- filehash_cache_check.py
+++ filehash_cache_check.py
@@ -167,10 +167,12 @@ def grab_from_cache(action=None, success=None, container=None, results=None, han
     success, message, matches = phantom.get_list(list_name='virus_total_cache', values=filtered_artifacts_item_1_0[0])
     if success:
         malicious_count = matches.get('matches')[0].get('value')[2]
+        success_2, message_2 = phantom.delete_from_list(list_name="virus_total_cache", value=filtered_artifacts_item_1_0[0], remove_row=True)
         success_1, message_1 = phantom.add_list(list_name="virus_total_cache", values=[matches.get('matches')[0].get('value')[0], matches.get('matches')[0].get('value')[1], matches.get('matches')[0].get('value')[2], int(matches.get('matches')[0].get('value')[3]) + 1])
         
     phantom.debug('phantom.get_list results: success: {}, message: {}, malicious_count: {}'.format(success, message, malicious_count))# Write your custom code here...
     grab_from_cache__malicious_count = malicious_count
+    ####################################################
     ####################################################
     ####################################################
     ####################################################

34ae7a3341dd7823021b1a6d58f6944adb98df2b
admin <root@localhost>
2022-03-03 19:58:28 +0000

    notes and descriptions

--- filehash_cache_check.py
+++ filehash_cache_check.py
@@ -7,13 +7,13 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
     
-    # call 'filter_1' block
-    filter_1(container=container)
+    # call 'check_field' block
+    check_field(container=container)
 
     return
 
-def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('filter_1() called')
+def check_field(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('check_field() called')
 
     # collect filtered artifact ids for 'if' condition 1
     matched_artifacts_1, matched_results_1 = phantom.condition(
@@ -21,7 +21,7 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
         conditions=[
             ["artifact:*.cef.fileHash", "!=", ""],
         ],
-        name="filter_1:condition_1")
+        name="check_field:condition_1")
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
@@ -32,7 +32,7 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
 def check_list(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('check_list() called')
     
-    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:filter_1:condition_1:artifact:*.cef.fileHash'])
+    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:check_field:condition_1:artifact:*.cef.fileHash'])
     filtered_artifacts_item_1_0 = [item[0] for item in filtered_artifacts_data_1]
 
     check_list__inList = None
@@ -100,19 +100,19 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
         return
 
     # call connected blocks for 'else' condition 2
-    file_reputation_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+    file_reputation(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
 
     return
 
-def file_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('file_reputation_1() called')
+def file_reputation(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('file_reputation() called')
 
-    # collect data for 'file_reputation_1' call
-    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:filter_1:condition_1:artifact:*.cef.fileHash', 'filtered-data:filter_1:condition_1:artifact:*.id'])
+    # collect data for 'file_reputation' call
+    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:check_field:condition_1:artifact:*.cef.fileHash', 'filtered-data:check_field:condition_1:artifact:*.id'])
 
     parameters = []
     
-    # build parameters list for 'file_reputation_1' call
+    # build parameters list for 'file_reputation' call
     for filtered_artifacts_item_1 in filtered_artifacts_data_1:
         if filtered_artifacts_item_1[0]:
             parameters.append({
@@ -121,14 +121,14 @@ def file_reputation_1(action=None, success=None, container=None, results=None, h
                 'context': {'artifact_id': filtered_artifacts_item_1[1]},
             })
 
-    phantom.act(action="file reputation", parameters=parameters, assets=['virustotal'], callback=update_cache, name="file_reputation_1")
+    phantom.act(action="file reputation", parameters=parameters, assets=['virustotal'], callback=update_cache, name="file_reputation")
 
     return
 
 def grab_from_cache(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('grab_from_cache() called')
     
-    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:filter_1:condition_1:artifact:*.cef.fileHash'])
+    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:check_field:condition_1:artifact:*.cef.fileHash'])
     filtered_artifacts_item_1_0 = [item[0] for item in filtered_artifacts_data_1]
 
     grab_from_cache__malicious_count = None
@@ -229,7 +229,7 @@ def decision_4(action=None, success=None, container=None, results=None, handle=N
         container=container,
         action_results=results,
         conditions=[
-            ["file_reputation_1:action_result.summary.malicious", ">", 0],
+            ["file_reputation:action_result.summary.malicious", ">", 0],
         ])
 
     # call connected blocks if condition 1 matched
@@ -281,8 +281,8 @@ def severity_low_pin_live(action=None, success=None, container=None, results=Non
 def update_cache(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('update_cache() called')
     
-    results_data_1 = phantom.collect2(container=container, datapath=['file_reputation_1:action_result.summary.malicious'], action_results=results)
-    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:filter_1:condition_1:artifact:*.cef.fileHash'])
+    results_data_1 = phantom.collect2(container=container, datapath=['file_reputation:action_result.summary.malicious'], action_results=results)
+    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:check_field:condition_1:artifact:*.cef.fileHash'])
     filtered_artifacts_item_1_0 = [item[0] for item in filtered_artifacts_data_1]
     results_item_1_0 = [item[0] for item in results_data_1]
 
